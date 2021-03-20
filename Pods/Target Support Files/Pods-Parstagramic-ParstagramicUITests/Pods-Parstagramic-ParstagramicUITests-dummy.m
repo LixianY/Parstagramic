82eab3dba60cb5a02b7d117de90734ad2ed1500a
LixianY <yanlixian47@gmail.com>
2021-03-20 14:01:47 -0400

    Initial Commit

---
+++ Pods/Target Support Files/Pods-Parstagramic-ParstagramicUITests/Pods-Parstagramic-ParstagramicUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Parstagramic_ParstagramicUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Parstagramic_ParstagramicUITests
+@end
---
+++ Pods/Target Support Files/Pods-ParstagramicTests/Pods-ParstagramicTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ParstagramicTests : NSObject
+@end
+@implementation PodsDummy_Pods_ParstagramicTests
+@end
